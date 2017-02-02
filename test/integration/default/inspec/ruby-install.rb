ef2f559724afc6339351afbed327e2c34ab2092a
Marco Ferrari <ferrari.marco@gmail.com>
2017-02-02 11:12:47 +0100

    Cleanup leftovers. Closes #4

--- test/integration/default/inspec/ruby-install.rb
+++ test/integration/default/inspec/ruby-install.rb
@@ -1,12 +1,26 @@
 control "ansible-role-ruby-install" do
   title "ansible-role-ruby-install role check"
 
-  describe file('/etc/bash_aliases') do
+  describe directory('/usr/local/share/doc/ruby-install-0.6.0') do
       it { should exist }
-      it { should be_file }
       it { should be_owned_by 'root' }
       it { should be_grouped_into 'root' }
       it { should be_readable.by_user('root') }
-      its('content') { should match(%r{alias test-alias='test-command'}) }
-    end
+  end
+
+  describe file('/tmp/ruby-install-0.6.0.tar.gz') do
+      it { should_not exist }
+  end
+
+  describe directory('/tmp/ruby-install-0.6.0') do
+      it { should_not exist }
+  end
+
+  describe file('/usr/local/bin/ruby-install') do
+      it { should exist }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_readable.by_user('root') }
+      its('mode') { should cmp '0755' }
+  end
 end
